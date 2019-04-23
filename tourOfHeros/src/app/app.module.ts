6d24933a5fa08ed93de1f1f516fc6f243d505782
AlFalah <taiebalfalah@gmail.com>
2019-04-23 22:33:06 +0200

    delete Home

--- tourOfHeros/src/app/app.module.ts
+++ tourOfHeros/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { DistrosComponent } from './distros/distros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     DistrosComponent
   ],
   imports: [
