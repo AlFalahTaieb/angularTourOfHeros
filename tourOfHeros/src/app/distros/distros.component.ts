0d3e8b082d38178badf15c9c1e6da80646f2ac71
AlFalah <taiebalfalah@gmail.com>
2019-04-24 15:43:43 +0200

    List of all distros

--- tourOfHeros/src/app/distros/distros.component.ts
+++ tourOfHeros/src/app/distros/distros.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Distro } from './distro';
+import { DISTROS } from './exp-distros';
 @Component({
   selector: 'app-distros',
   templateUrl: './distros.component.html',
   styleUrls: ['./distros.component.css']
 })
 export class DistrosComponent implements OnInit {
-  distro: Distro = {
-    id: 1,
-    name: 'Debian'
-  };
+  distros = DISTROS;
 
+  selectedDistro: Distro;
+  onSelect(distro: Distro) {
+    console.log(this.selectedDistro = distro);
+    this.selectedDistro = distro;
+  }
   constructor() {
 
 
   }
 
   ngOnInit() {
+
   }
 
 }
---
+++ tourOfHeros/src/app/distros/exp-distros.ts
@@ -0,0 +1,15 @@
+import { Distro } from './distro';
+
+export const DISTROS: Distro[] = [
+    { id: 11, name: 'Ubuntu' },
+    { id: 12, name: 'Arch' },
+    { id: 13, name: 'Void' },
+    { id: 14, name: 'Antergos' },
+    { id: 15, name: 'Manjaro' },
+    { id: 16, name: 'Kali Linux' },
+    { id: 17, name: 'PopOS!' },
+    { id: 18, name: 'Fedora' },
+    { id: 19, name: 'ReactOs' },
+    { id: 20, name: 'OpenSuse' }
+
+];
