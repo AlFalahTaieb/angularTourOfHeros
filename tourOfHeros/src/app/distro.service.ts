e07c25206a634cfdcf18776177536bc4be5c1f23
AlFalah <taiebalfalah@gmail.com>
2019-04-25 17:12:54 +0200

    add service

--- tourOfHeros/src/app/distro.service.ts
+++ tourOfHeros/src/app/distro.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Distro } from './distros/distro';
+import { DISTROS } from './distros/exp-distros';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DistroService {
-
+  getDistros(): Distro[] {
+    return DISTROS;
+  }
   constructor() { }
 }
--- tourOfHeros/src/app/distros/distros.component.ts
+++ tourOfHeros/src/app/distros/distros.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Distro } from './distro';
 import { DISTROS } from './exp-distros';
+import { DistroService } from '../distro.service';
 @Component({
   selector: 'app-distros',
   templateUrl: './distros.component.html',
   styleUrls: ['./distros.component.css']
 })
 export class DistrosComponent implements OnInit {
-  distros = DISTROS;
+
 
   selectedDistro: Distro;
   onSelect(distro: Distro) {
     console.log(this.selectedDistro = distro);
     this.selectedDistro = distro;
   }
-  constructor() {
-
+  constructor(private distroService: DistroService) {
 
   }
 
